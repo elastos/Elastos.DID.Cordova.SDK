e808dc75dbabe994a769687a43ce966b9a7a6152
Benjamin Piette <benjamin@findizer.fr>
2019-11-23 16:49:02 +0800

    Converted DID plugin to TS

--- www/did.ts
+++ www/did.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 Elastos Foundation
+ * Copyright (c) 2019-2020 Elastos Foundation
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -20,51 +20,45 @@
  * SOFTWARE.
  */
 
-var exec = require('cordova/exec');
+var exec = cordova.exec;
 
-function VerifiableCredential() {
-    this.objId  = null;
-    this.clazz  = 5;
-}
+class VerifiableCredentialImpl implements VerifiableCredential {
+    objId = null;
+    clazz = 5;
+    info: any = null;
 
-VerifiableCredential.prototype = {
-    constructor: VerifiableCredential,
-
-    getFragment: function(onSuccess, onError) {
+    getFragment(onSuccess: (data: any) => void, onError?: (err: any) => void) {
         exec(onSuccess, onError, 'DIDPlugin', 'getFragment', [this.objId]);
-    },
+    }
 
-    getType: function(onSuccess, onError) {
+    getType(onSuccess: (data: any) => void, onError?: (err: any) => void) {
         exec(onSuccess, onError, 'DIDPlugin', 'getType', [this.objId]);
-    },
+    }
 
-    getIssuer: function(onSuccess, onError) {
+    getIssuer(onSuccess: (data: any) => void, onError?: (err: any) => void) {
         exec(onSuccess, onError, 'DIDPlugin', 'getIssuer', [this.objId]);
-    },
+    }
 
-    getIssuanceDate: function(onSuccess, onError) {
+    getIssuanceDate(onSuccess: (data: any) => void, onError?: (err: any) => void) {
         exec(onSuccess, onError, 'DIDPlugin', 'getIssuanceDate', [this.objId]);
-    },
+    }
 
-    getExpirationDate: function(onSuccess, onError) {
+    getExpirationDate(onSuccess: (data: any) => void, onError?: (err: any) => void) {
         exec(onSuccess, onError, 'DIDPlugin', 'getExpirationDate', [this.objId]);
-    },
+    }
 
-    getProperties: function(onSuccess, onError) {
+    getProperties(onSuccess: (data: any) => void, onError?: (err: any) => void) {
         exec(onSuccess, onError, 'DIDPlugin', 'getProperties', [this.objId]);
     }
 }
 
-function PublicKey() {
-    this.objId  = null;
-    this.clazz  = 4;
-}
+class PublicKeyImpl implements PublicKey {
+    objId = null;
+    clazz = 4;
+    plugin: DIDPlugin = null;
 
-PublicKey.prototype = {
-    constructor: PublicKey,
-
-    getController: function(onSuccess, onError) {
-        var did = new DID();
+    getController(onSuccess: (data: any) => void, onError?: (err: any) => void) {
+        var did = new DIDImpl();
         var plugin = this.plugin;
 
         var _onSuccess = function(ret) {
@@ -74,53 +68,48 @@ PublicKey.prototype = {
         }
 
         exec(_onSuccess, onError, 'DIDPlugin', 'getController', [this.objId]);
-    },
+    }
 
-    getPublicKeyBase58: function(onSuccess, onError, method) {
+    getPublicKeyBase58(method: any, onSuccess: (data: any) => void, onError?: (err: any) => void) {
         exec(onSuccess, onError, 'DIDPlugin', 'getPublicKeyBase58', [this.objId, method]);
     }
 }
 
-function DID() {
-    this.objId  = null;
-    this.clazz  = 3;
-}
+class DIDImpl implements DID {
+    objId = null;
+    clazz = 3;
+    plugin: DIDPlugin = null;
 
-DID.prototype = {
-    constructor: DID,
-
-    getMethod: function(onSuccess, onError) {
+    getMethod(onSuccess: (data: any) => void, onError?: (err: any) => void) {
         exec(onSuccess, onError, 'DIDPlugin', 'getMethod', [this.objId]);
-    },
+    }
 
-    getMethodSpecificId: function(onSuccess, onError) {
+    getMethodSpecificId(onSuccess: (data: any) => void, onError?: (err: any) => void) {
         exec(onSuccess, onError, 'DIDPlugin', 'getMethodSpecificId', [this.objId]);
-    },
+    }
 
-    resolve: function(onSuccess, onError) {
+    resolve(onSuccess: (data: any) => void, onError?: (err: any) => void) {
         exec(onSuccess, onError, 'DIDPlugin', 'resolve', [this.objId]);
-    },
+    }
 
-    toString: function(onSuccess, onError) {
+    toString(onSuccess: (data: any) => void, onError?: (err: any) => void) {
         exec(onSuccess, onError, 'DIDPlugin', 'didToString', [this.objId]);
     }
 }
 
-function DIDDocument() {
-    this.objId  = null;
-    this.clazz  = 2;
-    this.DidString = "";
-}
+class DIDDocumentImpl implements DIDDocument {
+    objId  = null;
+    clazz  = 2;
+    DidString = "";
+    plugin: DIDPlugin = null;
+    did: DID = null;
 
-DIDDocument.prototype = {
-    constructor: DIDDocument,
-
-    setSubject: function(onSuccess, onError, subject) {
+    setSubject(subject: any, onSuccess?: (data: any) => void, onError?: (err: any) => void) {
         exec(onSuccess, onError, 'DIDPlugin', 'setSubject', [this.objId, subject]);
-    },
+    }
 
-    getSubject: function(onSuccess, onError) {
-        var did = new DID();
+    getSubject(onSuccess: (data: any) => void, onError?: (err: any) => void) {
+        var did = new DIDImpl();
         var plugin = this.plugin;
 
         var _onSuccess = function(ret) {
@@ -130,18 +119,18 @@ DIDDocument.prototype = {
         }
 
         exec(_onSuccess, onError, 'DIDPlugin', 'getSubject', [this.objId]);
-    },
+    }
 
-    getPublicKeyCount: function(onSuccess, onError) {
+    getPublicKeyCount(onSuccess: (data: any) => void, onError?: (err: any) => void) {
         exec(onSuccess, onError, 'DIDPlugin', 'getPublicKeyCount', [this.objId]);
-    },
+    }
 
-    getDefaultPublicKey: function(onSuccess, onError) {
+    getDefaultPublicKey(onSuccess: (data: any) => void, onError?: (err: any) => void) {
         exec(onSuccess, onError, 'DIDPlugin', 'getDefaultPublicKey', [this.objId]);
-    },
+    }
 
-    getPublicKey: function(onSuccess, onError, didString) {
-        var publicKey = new PublicKey();
+    getPublicKey(didString: string, onSuccess: (data: any) => void, onError?: (err: any) => void) {
+        var publicKey = new PublicKeyImpl();
         var plugin = this.plugin;
 
         var _onSuccess = function(ret) {
@@ -151,51 +140,47 @@ DIDDocument.prototype = {
         }
 
         exec(_onSuccess, onError, 'DIDPlugin', 'getPublicKey', [this.objId, didString]);
-    },
+    }
 
-    getPublicKeys: function(onSuccess, onError) {
+    getPublicKeys(onSuccess: (data: any) => void, onError?: (err: any) => void) {
         exec(onSuccess, onError, 'DIDPlugin', 'getPublicKeys', [this.objId]);
-    },
+    }
 
-    addCredential: function(onSuccess, onError, credentialId) {
+    addCredential(credentialId: any, onSuccess?: (data: any) => void, onError?: (err: any) => void) {
         exec(onSuccess, onError, 'DIDPlugin', 'addCredential', [this.objId, credentialId]);
-    },
+    }
 
-    getCredential: function(onSuccess, onError, credentialId) {
+    getCredential(credentialId: any, onSuccess?: (data: any) => void, onError?: (err: any) => void) {
         exec(onSuccess, onError, 'DIDPlugin', 'getCredential', [this.objId, credentialId]);
-    },
+    }
 
-    sign: function(onSuccess, onError, storepass, originString) {
-        exec(onSuccess, onError, 'DIDPlugin', 'sign', [this.objId, storepass, originString]);
-    },
+    sign(storePass: string, originString: string, onSuccess: (data: any) => void, onError?: (err: any) => void) {
+        exec(onSuccess, onError, 'DIDPlugin', 'sign', [this.objId, storePass, originString]);
+    }
 
-    verify: function(onSuccess, onError, signString, originString) {
+    verify(signString: string, originString: string, onSuccess: (data: any) => void, onError?: (err: any) => void) {
         exec(onSuccess, onError, 'DIDPlugin', 'verify', [this.objId, signString, originString]);
-    },
+    }
 }
 
-function DIDStore() {
-    this.objId  = null;
-    this.clazz  = 1;
-}
+class DIDStoreImpl implements DIDStore {
+    objId  = null;
+    clazz  = 1;
 
-DIDStore.prototype = {
-    constructor: DIDStore,
-
-    initPrivateIdentity: function(onSuccess, onError, mnemonic, passphrase, storepass, force) {
+    initPrivateIdentity(mnemonic: string, passphrase: string, storepass: string, force: Boolean, onSuccess: (data: any) => void, onError?: (err: any) => void) {
         exec(onSuccess, onError, 'DIDPlugin', 'initPrivateIdentity', [this.objId, mnemonic, passphrase, storepass, force]);
-    },
+    }
 
-    hasPrivateIdentity: function(onSuccess, onError) {
+    hasPrivateIdentity(onSuccess: (data: any) => void, onError?: (err: any) => void) {
         exec(onSuccess, onError, 'DIDPlugin', 'hasPrivateIdentity', [this.objId, ]);
-    },
+    }
 
-    deleteDid: function(onSuccess, onError, didString) {
+    deleteDid(didString: string, onSuccess: (data: any) => void, onError?: (err: any) => void) {
         exec(onSuccess, onError, 'DIDPlugin', 'deleteDid', [this.objId, didString]);
-    },
+    }
 
-    newDid: function(onSuccess, onError, passphrase, hint) {
-         var diddoc = new DIDDocument();
+    newDid(passphrase: string, hint: string, onSuccess: (data: any) => void, onError?: (err: any) => void) {
+         var diddoc = new DIDDocumentImpl();
 
          var _onSuccess = function(ret) {
              diddoc.objId = ret.id;
@@ -204,14 +189,14 @@ DIDStore.prototype = {
          }
 
          exec(_onSuccess, onError, 'DIDPlugin', 'newDid', [this.objId, passphrase, hint]);
-    },
+    }
 
-    listDids: function(onSuccess, onError, filter) {
+    listDids(filter: any, onSuccess: (data: any) => void, onError?: (err: any) => void) {
         exec(onSuccess, onError, 'DIDPlugin', 'listDids', [this.objId, filter]);
-    },
+    }
 
-    loadDid: function(onSuccess, onError, didString) {
-         var diddoc = new DIDDocument();
+    loadDid(didString: string, onSuccess: (data: any) => void, onError?: (err: any) => void) {
+         var diddoc = new DIDDocumentImpl();
 
          var _onSuccess = function(ret) {
              diddoc.objId = ret.id;
@@ -219,14 +204,14 @@ DIDStore.prototype = {
          }
 
          exec(_onSuccess, onError, 'DIDPlugin', 'loadDid', [this.objId, didString]);
-    },
+    }
 
-    publishDid: function(onSuccess, onError, didDocumentId, didUrlString, storepass) {
+    publishDid(didDocumentId: string, didUrlString: string, storepass: string, onSuccess?: (data: any) => void, onError?: (err: any) => void) {
         exec(onSuccess, onError, 'DIDPlugin', 'publishDid', [this.objId, didDocumentId, didUrlString, storepass]);
-    },
+    }
 
-    resolveDid: function(onSuccess, onError, didString) {
-        var diddoc = new DIDDocument();
+    resolveDid(didString: string, onSuccess: (data: any) => void, onError?: (err: any) => void) {
+        var diddoc = new DIDDocumentImpl();
 
         var _onSuccess = function(ret) {
             diddoc.objId = ret.id;
@@ -234,18 +219,18 @@ DIDStore.prototype = {
         }
 
         exec(_onSuccess, onError, 'DIDPlugin', 'resolveDid', [this.objId, didString]);
-    },
+    }
 
-    storeDid: function(onSuccess, onError, didDocumentId, hint) {
+    storeDid(didDocumentId: string, hint: string, onSuccess: (data: any) => void, onError?: (err: any) => void) {
         exec(onSuccess, onError, 'DIDPlugin', 'storeDid', [this.objId, didDocumentId, hint]);
-    },
+    }
 
-    updateDid: function(onSuccess, onError, didDocumentId, didUrlString, storepass) {
+    updateDid(didDocumentId: string, didUrlString: string, storepass: string, onSuccess?: (data: any) => void, onError?: (err: any) => void) {
         exec(onSuccess, onError, 'DIDPlugin', 'updateDid', [this.objId, didDocumentId, didUrlString, storepass]);
-    },
+    }
 
-    createCredential: function(onSuccess, onError, didString, credentialId, type, expirationDate, properties, passphrase) {
-        var credential = new VerifiableCredential();
+    createCredential(didString: string, credentialId: string, type: any, expirationDate: any, properties: any, passphrase: string, onSuccess: (data: any) => void, onError?: (err: any) => void) {
+        var credential = new VerifiableCredentialImpl();
 
         var _onSuccess = function(ret) {
             credential.objId = ret.id;
@@ -254,18 +239,18 @@ DIDStore.prototype = {
 
         exec(_onSuccess, onError, 'DIDPlugin', 'CreateCredential',
                    [this.objId, didString, credentialId, type, expirationDate, properties, passphrase]);
-    },
+    }
 
-    deleteCredential: function(onSuccess, onError, didString, didUrlString) {
+    deleteCredential(didString: string, didUrlString: string, onSuccess?: (data: any) => void, onError?: (err: any) => void) {
         exec(onSuccess, onError, 'DIDPlugin', 'deleteCredential', [this.objId, didString, didUrlString]);
-    },
+    }
 
-    listCredentials: function(onSuccess, onError, didString) {
+    listCredentials(didString: string, onSuccess: (data: any) => void, onError?: (err: any) => void) {
         exec(onSuccess, onError, 'DIDPlugin', 'listCredentials', [this.objId, didString]);
-    },
+    }
 
-    loadCredential: function(onSuccess, onError, didString, didUrlString) {
-        var credential = new VerifiableCredential();
+    loadCredential(didString: string, didUrlString: string, onSuccess: (data: any) => void, onError?: (err: any) => void) {
+        var credential = new VerifiableCredentialImpl();
 
         var _onSuccess = function(ret) {
             credential.objId = ret.id;
@@ -274,61 +259,41 @@ DIDStore.prototype = {
         }
 
         exec(_onSuccess, onError, 'DIDPlugin', 'loadCredential', [this.objId, didString, didUrlString]);
-    },
+    }
 
-    storeCredential: function(onSuccess, onError, credentialId) {
+    storeCredential(credentialId: string, onSuccess?: (data: any) => void, onError?: (err: any) => void) {
         exec(onSuccess, onError, 'DIDPlugin', 'storeCredential', [this.objId, credentialId]);
     }
 }
 
-function DIDPlugin() {
-    this.DIDStoreFilter = {
-        DID_HAS_PRIVATEKEY: 0,
-        DID_NO_PRIVATEKEY: 1,
-        DID_ALL: 2,
-    };
-
-    this.Mnemonic = {
-        ENGLISH: 0,
-        FRENCH: 1,
-        SPANISH: 2,
-        CHINESE_SIMPLIFIED: 3,
-        CHINESE_TRADITIONAL: 4,
-        JAPANESE: 5,
+class DIDPluginImpl implements DIDPlugin {
+    constructor() {
+        Object.freeze(DIDPluginImpl.prototype);
+        Object.freeze(DIDStoreImpl.prototype);
+        Object.freeze(DIDDocumentImpl.prototype);
+        Object.freeze(DIDImpl.prototype);
+        Object.freeze(PublicKeyImpl.prototype);
+        Object.freeze(VerifiableCredentialImpl.prototype);
+    
+        exec(function () {}, null, 'DIDPlugin', 'initVal', []);
     }
 
-    Object.freeze(DIDPlugin.prototype);
-    Object.freeze(DIDStore.prototype);
-    Object.freeze(DIDDocument.prototype);
-    Object.freeze(DID.prototype);
-    Object.freeze(PublicKey.prototype);
-    Object.freeze(VerifiableCredential.prototype);
-
-    Object.freeze(this.DIDStoreFilter);
-    Object.freeze(this.Mnemonic);
-
-    exec(function () {}, null, 'DIDPlugin', 'initVal', []);
-}
-
-DIDPlugin.prototype = {
-    constructor: DIDPlugin,
-
-    getVersion: function(onSuccess, onError) {
+    getVersion(onSuccess: (data: any)=>void, onError?: (err: any)=>void) {
         exec(onSuccess, onError, 'DIDPlugin', 'getVersion', []);
-    },
+    }
 
-    initDidStore: function(onSuccess, onError, location, passphrase) {
-        var didStore = new DIDStore();
+    initDidStore(location: string, passphrase: string, onSuccess?: (data: any)=>void, onError?: (err: any)=>void) {
+        var didStore = new DIDStoreImpl();
 
-        var _onSuccess = function(ret) {
+        var _onSuccess = function(ret: any) {
             didStore.objId = ret.id;
             if (onSuccess) onSuccess(didStore);
         }
         exec(_onSuccess, onError, 'DIDPlugin', 'initDidStore', [location, passphrase]);
-    },
+    }
 
-    createDIDDocumentFromJson: function(onSuccess, onError, json) {
-        var didDocument = new DIDDocument();
+    createDIDDocumentFromJson(json: any, onSuccess?: (data: any)=>void, onError?: (err: any)=>void){
+        var didDocument = new DIDDocumentImpl();
 
         var _onSuccess = function(ret) {
             didDocument.objId = ret.id;
@@ -336,16 +301,15 @@ DIDPlugin.prototype = {
         }
 
         exec(_onSuccess, onError, 'DIDPlugin', 'CreateDIDDocumentFromJson', [json]);
-    },
+    }
 
-    generateMnemonic: function(onSuccess, onError, language) {
+    generateMnemonic(language: any, onSuccess: (data: any)=>void, onError?: (err: any)=>void) {
         exec(onSuccess, onError, 'DIDPlugin', 'generateMnemonic', [language]);
-    },
+    }
 
-    isMnemonicValid: function(onSuccess, onError, language, mnemonic) {
+    isMnemonicValid(language: any, mnemonic: string, onSuccess: (data: any)=>void, onError?: (err: any)=>void) {
         exec(onSuccess, onError, 'DIDPlugin', 'isMnemonicValid', [language, mnemonic]);
-    },
-
+    }
 }
 
-module.exports = new DIDPlugin();
+export = new DIDPluginImpl();
---
+++ www/types.d.ts
@@ -0,0 +1,102 @@
+/*
+ * Copyright (c) 2019 Elastos Foundation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+declare type DIDStoreFilter = {
+    DID_HAS_PRIVATEKEY: 0,
+    DID_NO_PRIVATEKEY: 1,
+    DID_ALL: 2
+}
+
+declare type Mnemonic = {
+    ENGLISH: 0,
+    FRENCH: 1,
+    SPANISH: 2,
+    CHINESE_SIMPLIFIED: 3,
+    CHINESE_TRADITIONAL: 4,
+    JAPANESE: 5
+}
+
+interface VerifiableCredential {
+    // TODO: define onSuccess and onError? callbacks parameters with more accurate types
+    getFragment: (onSuccess: (data: any)=>void, onError?: (err: any)=>void)=>void;
+    getType: (onSuccess: (data: any)=>void, onError?: (err: any)=>void)=>void;
+    getIssuer: (onSuccess: (data: any)=>void, onError?: (err: any)=>void)=>void;
+    getIssuanceDate: (onSuccess: (data: any)=>void, onError?: (err: any)=>void)=>void;
+    getExpirationDate: (onSuccess: (data: any)=>void, onError?: (err: any)=>void)=>void;
+    getProperties: (onSuccess: (data: any)=>void, onError?: (err: any)=>void)=>void;
+}
+
+interface PublicKey {
+    // TODO: define onSuccess and onError? callbacks parameters with more accurate types
+    getController: (onSuccess: (data: any)=>void, onError?: (err: any)=>void)=>void;
+    getPublicKeyBase58: (method: any, onSuccess: (data: any)=>void, onError?: (err: any)=>void)=>void; // TODO: define "method" type
+}
+
+interface DID {
+    // TODO: define onSuccess and onError? callbacks parameters with more accurate types
+    getMethod: (onSuccess: (data: any)=>void, onError?: (err: any)=>void)=>void;
+    getMethodSpecificId: (onSuccess: (data: any)=>void, onError?: (err: any)=>void)=>void;
+    resolve: (onSuccess: (data: any)=>void, onError?: (err: any)=>void)=>void;
+    toString: (onSuccess: (data: any)=>void, onError?: (err: any)=>void)=>void;
+}
+
+interface DIDDocument {
+    // TODO: define onSuccess and onError? callbacks parameters with more accurate types
+    setSubject: (subject: any, onSuccess?: (data: any)=>void, onError?: (err: any)=>void)=>void; // TODO: "subject" type
+    getSubject: (onSuccess: (data: any)=>void, onError?: (err: any)=>void)=>void;
+    getPublicKeyCount: (onSuccess: (data: any)=>void, onError?: (err: any)=>void)=>void;
+    getDefaultPublicKey: (onSuccess: (data: any)=>void, onError?: (err: any)=>void)=>void;
+    getPublicKey: (didString: string, onSuccess: (data: any)=>void, onError?: (err: any)=>void)=>void;
+    getPublicKeys: (onSuccess: (data: any)=>void, onError?: (err: any)=>void)=>void;
+    addCredential: (credentialId: any, onSuccess?: (data: any)=>void, onError?: (err: any)=>void)=>void; // TODO "credentialId" type
+    getCredential: (credentialId: any, onSuccess?: (data: any)=>void, onError?: (err: any)=>void)=>void; // TODO "credentialId" type
+    sign: (storePass: string, originString: string, onSuccess: (data: any)=>void, onError?: (err: any)=>void)=>void;  // TODO: What is "originString" ?
+    verify: (signString: string, originString: string, onSuccess: (data: any)=>void, onError?: (err: any)=>void)=>void;
+}
+
+interface DIDStore {
+    // TODO: define onSuccess and onError? callbacks parameters with more accurate types
+    initPrivateIdentity: (mnemonic: string, passphrase: string, storepass: string, force: Boolean, onSuccess: (data: any)=>void, onError?: (err: any)=>void)=>void;
+    hasPrivateIdentity: (onSuccess: (data: any)=>void, onError?: (err: any)=>void)=>void;
+    deleteDid: (didString: string, onSuccess: (data: any)=>void, onError?: (err: any)=>void)=>void;
+    newDid: (passphrase: string, hint: string, onSuccess: (data: any)=>void, onError?: (err: any)=>void)=>void;
+    listDids: (filter: any, onSuccess: (data: any)=>void, onError?: (err: any)=>void)=>void; // TODO: "filter" type
+    loadDid: (didString: string, onSuccess: (data: any)=>void, onError?: (err: any)=>void)=>void;
+    publishDid: (didDocumentId: string, didUrlString: string, storepass: string, onSuccess?: (data: any)=>void, onError?: (err: any)=>void)=>void;
+    resolveDid: (didString: string, onSuccess: (data: any)=>void, onError?: (err: any)=>void)=>void;
+    storeDid: (didDocumentId: string, hint:string, onSuccess: (data: any)=>void, onError?: (err: any)=>void)=>void;
+    updateDid: (didDocumentId: string, didUrlString: string, storepass: string, onSuccess?: (data: any)=>void, onError?: (err: any)=>void)=>void;
+    createCredential: (didString: string, credentialId: string, type: any, expirationDate: any, properties: any, passphrase: string, onSuccess: (data: any)=>void, onError?: (err: any)=>void)=>void; // TODO: types for all "any"
+    deleteCredential: (didString: string, didUrlString: string, onSuccess?: (data: any)=>void, onError?: (err: any)=>void)=>void;
+    listCredentials: (didString: string, onSuccess: (data: any)=>void, onError?: (err: any)=>void)=>void;
+    loadCredential: (didString: string, didUrlString: string, onSuccess: (data: any)=>void, onError?: (err: any)=>void)=>void;
+    storeCredential: (credentialId: string, onSuccess?: (data: any)=>void, onError?: (err: any)=>void)=>void;
+}
+
+interface DIDPlugin {
+    // TODO: define onSuccess and onError? callbacks parameters with more accurate types
+    getVersion: (onSuccess: (data: any)=>void, onError?: (err: any)=>void)=>void;
+    initDidStore: (location: string, passphrase: string, onSuccess?: (data: any)=>void, onError?: (err: any)=>void)=>void;
+    createDIDDocumentFromJson: (json: any, onSuccess: (data: any)=>void, onError?: (err: any)=>void)=>void; // TODO: "json" type
+    generateMnemonic: (language: any, onSuccess: (data: any)=>void, onError?: (err: any)=>void)=>void; // TODO: "language" type
+    isMnemonicValid: (language: any, mnemonic: string, onSuccess: (data: any)=>void, onError?: (err: any)=>void)=>void; // TODO: "language" type
+}
